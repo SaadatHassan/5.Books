e02d2c9ba30b22781032ce2f8cc1da33d2549294
Saadat Hassan <saadat.hassan@rsmus.com>
2024-03-20 16:13:45 -0400

    added components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
+import { useState } from "react";
+import BookCreate from "./components/BookCreate";
+
 function App() {
+  const [books, setBooks] = useState([]);
+
+  const createBook = (title) => {
+    console.log(`Need to add this book with ${title}`);
+    const updatedBooks = [...books, { id: 1, title: title }];
+    setBooks(updatedBooks);
+  };
   return (
     <>
-      <div>books</div>
+      <div>
+        {books.length}
+        <BookCreate onCreate={createBook} />
+      </div>
     </>
   );
 }
